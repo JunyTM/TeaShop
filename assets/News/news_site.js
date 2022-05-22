d1583cb29231e842d9665485fac0bc19341f7c41
ManhKim <manhtokim@gmail.com>
2022-05-22 13:21:02 +0700

    update news site

--- assets/News/news_site.js
+++ assets/News/news_site.js
@@ -43,10 +43,11 @@ function GetDetail(res) {
            rootElement.remove();
            let html = `
             <div class="siteDetail"> 
-                <h1>${index.attributes.Title}</h1>
+                
                 <img src=${index.attributes.PhotoURL} alt="" />
                 <div class="content-site">
-                <pre>${index.attributes.Content}</pre>
+                <h2>${index.attributes.Title}</h2>
+                <p>${index.attributes.Content}</p>
                 </div>
             </div>
            `;
--- assets/Product/product.js
+++ assets/Product/product.js
@@ -1,4 +1,8 @@
 const rootElement = document.getElementById('root');
+const cafeElement = document.querySelector('#root .cafe');
+const fruitElement = document.querySelector('#root .fruit');
+const teaElement = document.querySelector('#root .tea');
+const cakeElement = document.querySelector('#root .cake');
 const rootDetail = document.getElementById('detail');
 const ProductAPI = 'http://localhost:1337/api/productions';
 
@@ -20,8 +24,10 @@ function GetProductions(callback) {
 }
 
 function RenderProduct(res) {
-    let htmls = res.data.map(data => {
+    //reder cafes
+    let cafes = res.data.map(data => {
         console.log(data);
+        if (data.attributes.Class == "cafe") 
         return `
         <li class="item-${data.id}">
             <h2>${data.attributes.Name}</h2>
@@ -29,5 +35,68 @@ function RenderProduct(res) {
         </li>
         `;
     });
-    rootElement.innerHTML = htmls.join('');
+    cafeElement.innerHTML = cafes.join('');
+
+    //render fruits  
+    let fruits = res.data.map(data => {
+        console.log(data);
+        if (data.attributes.Class == "fruit") 
+        return `
+        <li class="item-${data.id}">
+            <h2>${data.attributes.Name}</h2>
+            <img src=${data.attributes.PhotoURL} alt="" />
+        </li>
+        `;
+    });
+    fruitElement.innerHTML = fruits.join('');
+
+    //render teas
+    let teas = res.data.map(data => {
+        console.log(data);
+        if (data.attributes.Class == "tea") 
+        return `
+        <li class="item-${data.id}">
+            <h2>${data.attributes.Name}</h2>
+            <img src=${data.attributes.PhotoURL} alt="" />
+        </li>
+        `;
+    });
+    teaElement.innerHTML = teas.join('');
+
+    //render cakes
+    let cakes = res.data.map(data => {
+        console.log(data);
+        if (data.attributes.Class == "cake") 
+        return `
+        <li class="item-${data.id}">
+            <h2>${data.attributes.Name}</h2>
+            <img src=${data.attributes.PhotoURL} alt="" />
+        </li>
+        `;
+    });
+    cakeElement.innerHTML = cakes.join('');
 }
+
+function GetDetail(res) {
+    
+    for (let index of res.data) {
+        const itemSite = document.querySelector('.item-'+index.id);
+        
+        itemSite.onclick = function () {
+           rootElement.remove();
+           let html = `
+            <div class="itemDetail"> 
+                
+                <img src=${index.attributes.PhotoURL} alt="" />
+                <div class="content-site">
+                <h2>${index.attributes.Title}</h2>
+                <p>${index.attributes.Content}</p>
+                </div>
+            </div>
+           `;
+           rootDetail.innerHTML = html;
+        }
+    }
+
+}
+
