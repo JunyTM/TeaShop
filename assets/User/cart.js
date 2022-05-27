b84eb40cb7ef4f814a5b58f1e15ffac0119f330e
dieulinh205 <dieulinh20052002@gmail.com>
2022-05-27 23:52:25 +0700

    gio hang - Linh Ngo

--- assets/User/cart.js
+++ assets/User/cart.js
@@ -2,7 +2,7 @@
 var remove_cart = document.getElementsByClassName("btn-danger");
 for (var i = 0; i < remove_cart.length; i++) {
   var button = remove_cart[i]
-  button.addEventListener("click", function () {
+  button.addEventListener("click", function (event) {
     var button_remove = event.target
     button_remove.parentElement.parentElement.remove()
   })
